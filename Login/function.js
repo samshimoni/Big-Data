85943fe03987ee40e11164bc795e8652bb5beb81
samshimoni <33809931+samshimoni@users.noreply.github.com>
2019-08-20 08:46:24 -0400

    Add files via upload

---
+++ Login/function.js
@@ -0,0 +1,4 @@
+function al(){
+
+  alert("test test");
+}
---
+++ Login/myscript.js
@@ -0,0 +1,8 @@
+
+function checkPassword(){
+  var username = document.login.usrnme.value;
+  var password = document.login.pswrd.value;
+
+
+  alert(username);
+}
