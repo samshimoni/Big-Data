b018658047b66792f6699d1a1b9240d5a191a390
samshimoni <33809931+samshimoni@users.noreply.github.com>
2019-08-28 16:36:08 -0400

    Add files via upload

--- Server Side/PythonServer/hdfsPySpark.py
+++ Server Side/PythonServer/hdfsPySpark.py
@@ -9,6 +9,7 @@ from pyspark.sql import Window
 import pymongo
 from pymongo import MongoClient
 
+
 def collectDataFromHDFS():
     conf = SparkConf().setAppName("myFirstApp").setMaster("local")
     sc = SparkContext(conf=conf)
@@ -22,12 +23,46 @@ def collectDataFromHDFS():
         #print(x['path'])
         csvDataframe = sqlContext.read.format("csv").option("header", "true").option("inferschema", "true").option("mode", "DROPMALFORMED").load("hdfs://localhost:9000"+x['path'])
         ls.append(csvDataframe)
+
+    df = ls[1].union(ls[0])
+    for i in range(2, 6):
+        df = df.union(ls[i])
+    #print(ls)
+    #df = ls[1][['item_name', 'date', 'price_for_UNIT/KG']].union(ls[0][['item_name', 'date', 'price_for_UNIT/KG']])
+    df.show()
+
+    sorted_list_df1 = df.groupBy("item_name").agg(F.collect_list(F.col("date")).alias("date_list")
+                                                  , F.collect_list(F.col("src")).alias("comp")
+                                                  , F.collect_list(F.col("price_for_UNIT/KG")).alias("prics_list")
+                                                  , F.collect_list(F.col("amount_KG")).alias("kg")
+                                                  , F.collect_list(F.col("amount_UNIT")).alias("unit")
+                                                  )
+
+    sorted_list_df1.show()
+
+    #sorted_list_df1.show()
+    sorted_list_df = df.groupBy("item_name").agg({"price_for_UNIT/KG": "min"})
+
+    sorted_list_df.show()
+    inner_join = df.join(sorted_list_df, sorted_list_df['item_name'] == df['item_name'], how='inner')
+    inner_join = inner_join.filter(inner_join["price_for_UNIT/KG"] == inner_join['min(price_for_UNIT/KG)'])
+
+    inner_join.show()
+    mapa = sorted_list_df.toJSON().map(lambda j: json.loads(j)).collect()
+    client = MongoClient('mongodb://samshimoni:1234@rest-shard-00-00-bu6z1.mongodb.net:27017,rest-shard-00-01-bu6z1.mongodb.net:27017,rest-shard-00-02-bu6z1.mongodb.net:27017/test?ssl=true&replicaSet=rest-shard-0&authSource=admin&retryWrites=true&w=majority')
+    db = client.get_database('BigData')
+    records = db.Receipts
+
+    #print(type(mapa[0]))
+    print(mapa)
+    print(type(mapa))
+    print(records.count_documents({}))
+    #records.insert_many(mapa)
+    #print(records.count_documents({}))
+
+
     return ls
 
-list = collectDataFromHDFS()
-df = list[1][['item_name', 'date', 'price_for_UNIT/KG']].union(list[0][['item_name', 'date', 'price_for_UNIT/KG']])
 
-df.show()
 
-sorted_list_df = df.groupBy("item_name").agg(F.collect_list(F.col("date")).alias("date_list"), F.collect_list(F.col("price_for_UNIT/KG")).alias("prics_list"))
-sorted_list_df.show()
+
--- Server Side/PythonServer/server.py
+++ Server Side/PythonServer/server.py
@@ -1,43 +1,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from hdfsPySpark import collectDataFromHDFS
 
-
 COMMANDS_PORT = 4012
-
-...
-...
-
-'''
-def upload_file(path):
-    if not path == '/':  # if url is like '/query?filename='
-        filename = path.replace('/query?filename=', "", 1)
-        print(filename)
-        # wait for file to be uploaded
-        time.sleep(1)
-
-        # Check whether or not the file has been already uploaded
-        if filename not in hdfs_files:
-'''
-
-
-
-
-
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-
         print(self.address_string())
         self.send_response(200)
         self.end_headers()
-        #text_to_return = helloWorld()
         collectDataFromHDFS()
-
         #str1 = ''.join(map(str, ls))
         #self.wfile.write(str.encode(str1))
-
-        #text_to_return = upload_file(self.path)
-        # my_path =  self.path
         self.wfile.write(b'File Uploaded')
         #self.wfile.write(str.encode(text_to_return))
 
@@ -52,8 +25,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     #     # main_function(self.path)
     #     json_query_results = query_function(body)
     #     response.write(json_query_results)
-    #     response.write(body)
-    #     self.wfile.write(response.getvalue())        union(list[0][['item_name', 'date', 'price_for_UNIT/KG']])
+    #     response.write(body)mapa= sorted_list_df.toJSON().map(lambda j: json.loads(j)).collect()
+    #     self.wfile.write(response.getvalue())
+    #     union(list[0][['item_name', 'date', 'price_for_UNIT/KG']])
 
 print('Listening on port', COMMANDS_PORT)
 
